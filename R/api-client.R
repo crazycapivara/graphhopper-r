35f875545b18f1a43e3201c1c65d6ddaabae8585
skuethe <crazycapivara@gmail.com>
2019-03-27 19:38:16 +0100

    Add 'api_client'

--- R/api-client.R
+++ R/api-client.R
@@ -1,3 +1,12 @@
-api_client <- function(base_url = Sys.getenv("GH_API_URL")) {
-  list(api_url = base_url)
+#' API client
+#' @param base_url character scalar; API base url
+#' @param port numeric scalar; port
+#' @export
+api_client <- function(base_url = Sys.getenv("GH_API_URL"), port = NULL) {
+  structure(
+    function(path, query) {
+      httr::GET(base_url, port = port, path = path, query = query)
+    },
+    class = "api_client"
+  )
 }
--- R/route-api.R
+++ R/route-api.R
@@ -1,7 +1,8 @@
 #' Get the route for a given set of points.
+#' @param api_client api client, see \code{\link{api_client}}
 #' @param points list of points as [lat, lng] pairs
 #' @param ... additional query parameters, see \url{https://docs.graphhopper.com}
-#' @param sf whether to return route also as \code{sf} object
+#' @param sf logical scalar; whether to return route also as \code{sf} object
 #' @examples \dontrun{
 #'    start_point <- c(52.592204, 13.414307)
 #'    end_point <- c(52.539614, 13.364868)
@@ -9,14 +10,15 @@
 #'    route_get(list(start_point, end_point), locale = "de")
 #' }
 #' @export
-route_get <- function(points, ..., sf = TRUE) {
-  route_get_raw(points, ...) %>%
+route_get <- function(api_client, points, ..., sf = TRUE) {
+  route_get_raw(api_client, points, ...) %>%
     make_route_path(sf = sf)
 }
 
-route_get_raw <- function(points, ...) {
+route_get_raw <- function(api_client, points, ...) {
   query <- route_build_query(points, ...)
-  route_response <- httr::GET(get_api_url(), path = "route", query = query)
+  route_response <- api_client("route", query)
+  # route_response <- httr::GET(get_api_url(), path = "route", query = query)
   if (route_response$status != 200) {
     message("Something went wrong.")
     return(route_response)
