861f3cb4ba39cc7f446b113f89693b0f1cb5f7f6
skuethe <crazycapivara@gmail.com>
2020-11-16 17:15:35 +0100

    Refactor parsing routes

--- R/parse-route.R
+++ R/parse-route.R
@@ -1,22 +1,30 @@
+#' Extract the bounding box from response data
+#' @param data A \code{gh_route} or \link{gh_info} object.
+#' @seealso \link{gh_get_route}, \link{gh_get_info}
 #' @export
-gh_bbox <- function(content) {
-  UseMethod("gh_bbox", content)
+gh_bbox <- function(data) {
+  UseMethod("gh_bbox", data)
 }
 
+#' @name gh_bbox
 #' @export
-gh_bbox.gh_route <- function(content) {
-  content$paths[[1]]$bbox %>%
+gh_bbox.gh_route <- function(data) {
+  data$paths[[1]]$bbox %>%
     unlist()
 }
 
+#' @name gh_bbox
 #' @export
-gh_bbox.gh_info <- function(content) {
-  content$bbox %>% unlist()
+gh_bbox.gh_info <- function(data) {
+  data$bbox %>% unlist()
 }
 
+#' Extract instructions from route data
+#' @param data A \code{gh_route} object.
+#' @seealso \link{gh_get_route}
 #' @export
-gh_instructions <- function(content) {
-  path <- content$paths[[1]]
+gh_instructions <- function(data) {
+  path <- data$paths[[1]]
   lapply(path$instructions, function(x) {
     x$start_id <- x$interval[[1]]
     x$end_id <- x$interval[[2]]
@@ -27,8 +35,8 @@ gh_instructions <- function(content) {
 }
 
 #' @export
-gh_as_sf_points <- function(content) {
-  path <- content$paths[[1]]
+gh_as_sf_points <- function(data) {
+  path <- data$paths[[1]]
   points_sf <- googlePolylines::decode(path$points)[[1]][, c("lon", "lat")] %>%
     sf::st_as_sf(coords = c("lon", "lat"), crs = 4326)
   points_sf$gh_id <- 1:nrow(points_sf) - 1
