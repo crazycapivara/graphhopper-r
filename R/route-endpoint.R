53568e4647d251b3cb271d2820aef132c5fe6a58
skuethe <crazycapivara@gmail.com>
2020-11-16 12:08:41 +0100

    Add 'points_encoded' parameter

--- R/route-endpoint.R
+++ R/route-endpoint.R
@@ -1,8 +1,8 @@
 #' @export
-gh_get_route <- function(points, ..., response_only = FALSE) {
+gh_get_route <- function(points, ..., points_encoded = TRUE, response_only = FALSE) {
   points <- lapply(points, as_point_str)
   names(points) <- rep("point", length(points))
-  query <- c(points, ...)
+  query <- c(points, points_encoded = points_encoded, ...)
   response <- httr::GET(get_api_url(), path = "route", query = query)
   if (response_only) return(response)
 
